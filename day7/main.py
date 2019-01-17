b2aa8c31ec4ccb71af880f41c9f12170db9f596a
Lorcan Williamson <lorcan2k11@gmail.com>
2019-01-17 22:08:52 +0000

    got graph looking correct, but path length is wrong.

--- day7/main.py
+++ day7/main.py
@@ -1,11 +1,14 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import re
 
 with open('input.txt', 'r') as f:
     step_requirements = {}
+    edges = []
     for line in f:
         requirement, step = re.findall(r'(?<=tep )\w', line)
         step_requirements.setdefault(step, set()).add(requirement)
+        edges.append((requirement, step))
 start_steps = set([r for rs in step_requirements.values() for r in rs if r not in step_requirements])
 step_requirements.update([(start, set()) for start in start_steps])
 
@@ -21,25 +24,9 @@ while len(step_order_single) < len(step_requirements):
 # Part 2
 step_time = 0
 graph = nx.DiGraph()
-graph.add_node(0) # start point
-while len(graph.nodes()) < len(step_requirements):
-    for step, requirements in step_requirements.items():
-        if requirements == None:
-            continue
-        if requirements == set(): # add steps with no requirements
-            print("Adding step: ", step, requirements)
-            graph.add_node(step)
-            graph.add_edge(0, step, weight=ord(step)-64+step_time)
-            step_requirements[step] = None
-        elif requirements.issubset(set(graph.nodes())): # add graph with all requirements completed
-            print("Adding step: ", step, requirements)
-            graph.add_node(step)
-            graph.add_edge(list(requirements)[0], step, weight=ord(step)-64+step_time)
-            step_requirements[step] = None # remove node from list
-        for remaining_steps, remaining_req in step_requirements.items():
-            for node in graph.nodes():
-                if remaining_req != None and len(remaining_req) > 1 and node in remaining_req:
-                    step_requirements[remaining_steps].remove(node)
+for u, v in edges:
+    graph.add_edge(u, v, weight=ord(v)-64)
 time = nx.dag_longest_path_length(graph)
 nx.draw_networkx(graph)
+plt.show()
 print("Part 1:", step_order_single, "Part 2: ", time)
